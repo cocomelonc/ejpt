50fa0b59ead428b8aa4858d5ec72b5841f84f779
cocomelonc <cocomelonkz@gmail.com>
2021-03-26 15:23:55 +0600

    go example port scan add goroutines and channels

--- scanner.go
+++ scanner.go
@@ -3,17 +3,59 @@ package main
 import (
 	"fmt"
 	"net"
+	"sort"
 )
 
-func main() {
-	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("10.10.15.4:%d", i)
+func worker(ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("10.10.15.4:%d", p)
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
-			// either the port is filtered or closed
+			results <- 0
 			continue
 		}
 		conn.Close()
-		fmt.Printf("port %d: open\n", i)
+		results <- p
+	}
+}
+
+func main() {
+
+	// channel for ports
+	ports := make(chan int, 100)
+
+	// channel for results
+	results := make(chan int)
+
+	// for store results
+	var openports []int
+
+	// pool of workers
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	// send ports to be scanned
+	go func() {
+		for i := 1; i <= 65535; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 65535; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	// close the channels
+	close(ports)
+	close(results)
+
+	// sort open port numbers
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("port %d: open\n", port)
 	}
 }
